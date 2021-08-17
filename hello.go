c5c0c23b87fe3b70648bd010333166e2725a1016
yangkai <deveye@126.com>
2021-08-17 13:41:00 +0800

    awesome project

--- hello.go
+++ hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jordan-wright/email"
+	"net/smtp"
+)
 
-func main(){
-  fmt.Print("hello go! \n");
+func main() {
+	e := email.NewEmail()
+	e.From = "阿凯 <494040246@qq.com>"
+	e.To = []string{"799127463@qq.com"}
+	e.Subject = "发现惊天大秘密！"
+	e.Text = []byte("love 臭~")
+	err := e.Send("smtp.qq.com:25", smtp.PlainAuth("", "494040246@qq.com", "klbmkizobueacbbe", "smtp.qq.com"))
+	if err != nil {
+		panic(err)
+	}
 
 }
