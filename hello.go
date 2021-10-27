edc4b3bdc80b46d12cb86cf4dadc1626d149666d
kim <deveye@126.com>
2021-10-27 11:34:35 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	e.From = "kim <494040246@qq.com>"
 	e.To = []string{"494040246@qq.com"}
 	e.Subject = "发现惊天大秘密！"
-	e.Text = []byte("Hello World")
+	e.Text = []byte("Hello World!")
 	err := e.Send("smtp.qq.com:25", smtp.PlainAuth("", "494040246@qq.com", "klbmkizobueacbbe", "smtp.qq.com"))
 	if err != nil {
 		panic(err)
